149c637f7ae6c7aa2bd13872c15b90424c2cb6ad
Roberth Rios <tauro_roberth00@hotmail.com>
2022-04-02 00:22:11 -0500

    Simulación de tiempo de espera de petición HTTP en el botón "Ingresar"

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -104,16 +104,28 @@ class _LoginForm extends StatelessWidget {
                   horizontal: 80,
                   vertical: 15,
                 ),
-                child: const Text(
-                  'Ingresar',
-                  style: TextStyle(color: Colors.white),
+                child: Text(
+                  loginForm.isLoading ? 'Espere...' : 'Ingresar',
+                  style: const TextStyle(color: Colors.white),
                 ),
               ),
-              onPressed: () {
-                //TODO: Login Form
-                if (!loginForm.isValidForm()) return;
-                Navigator.pushReplacementNamed(context, 'home');
-              },
+              onPressed: loginForm.isLoading
+                  ? null
+                  : () async {
+                      //TODO: Login Form
+                      FocusScope.of(context).unfocus();
+
+                      if (!loginForm.isValidForm()) return;
+
+                      loginForm.isLoading = true;
+
+                      await Future.delayed(const Duration(seconds: 4));
+
+                      //TODO: validar si el login es correcto
+                      loginForm.isLoading = false;
+
+                      Navigator.pushReplacementNamed(context, 'home');
+                    },
             ),
           ],
         ),
