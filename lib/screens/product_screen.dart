b2cdf2b760b188d312ddc55fd6066a68b9903b0b
Roberth Rios <tauro_roberth00@hotmail.com>
2022-04-13 20:04:51 -0500

    Se cargo los datos de los productos en los formularios

--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -1,8 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:productos_app/interfaces/input_decoration.dart';
+import 'package:productos_app/providers/product_from_provider.dart';
+import 'package:productos_app/services/products_service.dart';
 import 'package:productos_app/widgets/widgets.dart';
+import 'package:provider/provider.dart';
 
 class ProductScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final productsService = Provider.of<ProductsService>(context);
+
+    return ChangeNotifierProvider(
+      create: (_) => ProductFormProvider(productsService.selectedProduct),
+      child: _ProductScreenBody(productsService: productsService),
+    );
+  }
+}
+
+class _ProductScreenBody extends StatelessWidget {
+  const _ProductScreenBody({
+    Key? key,
+    required this.productsService,
+  }) : super(key: key);
+
+  final ProductsService productsService;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,7 +33,9 @@ class ProductScreen extends StatelessWidget {
           children: [
             Stack(
               children: [
-                ProductImage(),
+                ProductImage(
+                  url: productsService.selectedProduct.picture,
+                ),
                 Positioned(
                   top: 60,
                   left: 20,
@@ -59,6 +83,9 @@ class ProductScreen extends StatelessWidget {
 class _ProductForm extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final productForm = Provider.of<ProductFormProvider>(context);
+    final product = productForm.product;
+
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 10),
       child: Container(
@@ -70,6 +97,13 @@ class _ProductForm extends StatelessWidget {
             children: [
               const SizedBox(height: 10),
               TextFormField(
+                initialValue: product.name,
+                onChanged: (value) => product.name = value,
+                validator: (value) {
+                  if (value == null || value.length < 1) {
+                    return 'El nombre es obligatorio';
+                  }
+                },
                 decoration: InputDecorations.authInputDecoration(
                   hintText: 'Nombre del producto',
                   labelText: 'Nombre: ',
@@ -77,6 +111,14 @@ class _ProductForm extends StatelessWidget {
               ),
               const SizedBox(height: 30),
               TextFormField(
+                initialValue: '${product.price}',
+                onChanged: (value) {
+                  if (double.tryParse(value) == null) {
+                    product.price = 0;
+                  } else {
+                    product.price = double.parse(value);
+                  }
+                },
                 keyboardType: TextInputType.number,
                 decoration: InputDecorations.authInputDecoration(
                   hintText: '\$150.25',
@@ -85,7 +127,7 @@ class _ProductForm extends StatelessWidget {
               ),
               const SizedBox(height: 30),
               SwitchListTile(
-                value: true,
+                value: product.available,
                 title: const Text('Disponible'),
                 activeColor: Colors.indigo,
                 onChanged: (value) {
--- lib/widgets/product_image.dart
+++ lib/widgets/product_image.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 
 class ProductImage extends StatelessWidget {
+  final String? url;
+
+  const ProductImage({Key? key, this.url}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -13,16 +17,23 @@ class ProductImage extends StatelessWidget {
         decoration: _buildBoxDecoration(),
         width: double.infinity,
         height: 450,
-        child: const ClipRRect(
-          borderRadius: BorderRadius.only(
+        child: ClipRRect(
+          borderRadius: const BorderRadius.only(
             topLeft: Radius.circular(45),
             topRight: Radius.circular(45),
           ),
-          child: FadeInImage(
-            placeholder: AssetImage('assets/jar-loading.gif'),
-            image: NetworkImage('https://via.placeholder.com/400x300/green'),
-            fit: BoxFit.cover,
-          ),
+          child: url == null
+              ? const Image(
+                  image: AssetImage(
+                    'assets/no-image.png',
+                  ),
+                  fit: BoxFit.cover,
+                )
+              : FadeInImage(
+                  placeholder: const AssetImage('assets/jar-loading.gif'),
+                  image: NetworkImage(url!),
+                  fit: BoxFit.cover,
+                ),
         ),
       ),
     );
